8c1cb586f0492bbee0e0f63ee28847fd07d57878
Ariel Vieira <arielvieira96@gmail.com>
2017-10-17 22:29:34 -0200

    Categoria DTO criada, e endpoint /categorias retornando todas categorias do banco

--- src/main/java/com/arielvieira/mc/resources/CategoriaResource.java
+++ src/main/java/com/arielvieira/mc/resources/CategoriaResource.java
@@ -3,6 +3,9 @@ package com.arielvieira.mc.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.arielvieira.mc.domain.Categoria;
+import com.arielvieira.mc.dto.CategoriaDTO;
 import com.arielvieira.mc.services.CategoriaService;
 
 @RestController
@@ -48,5 +52,13 @@ public class CategoriaResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {	
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream()
+			.map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
--- src/main/java/com/arielvieira/mc/services/CategoriaService.java
+++ src/main/java/com/arielvieira/mc/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.arielvieira.mc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -44,5 +46,9 @@ public class CategoriaService {
 			throw new DataIntegrityException("nao é possivel excluir categoria com produtos");
 		}
 		
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }

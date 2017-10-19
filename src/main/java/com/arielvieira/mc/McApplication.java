fac5f18116f2e38b27a8cca2f89f3b14db11e695
Ariel Vieira <arielvieira96@gmail.com>
2017-10-19 19:56:52 -0200

    Paginacao de categorias feita com parementros opcionais na requisicao

--- src/main/java/com/arielvieira/mc/McApplication.java
+++ src/main/java/com/arielvieira/mc/McApplication.java
@@ -64,6 +64,12 @@ public class McApplication implements CommandLineRunner{
 	public void run(String... arg0) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Informatica");
+		Categoria cat4 = new Categoria(null, "Escritorio");
+		Categoria cat5 = new Categoria(null, "Informatica");
+		Categoria cat6 = new Categoria(null, "Escritorio");
+		Categoria cat7 = new Categoria(null, "Informatica");
+		Categoria cat8 = new Categoria(null, "Escritorio");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora" , 800.00);
@@ -76,7 +82,7 @@ public class McApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		categoriaRepository.save(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8));
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");

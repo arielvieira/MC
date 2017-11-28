2497dc7dae52c747f6d3a156a2c9a87c35741cc8
Ariel Vieira <arielvieira96@gmail.com>
2017-11-28 17:03:46 -0200

    Autorizando endpoints para perfis especifificos

--- src/main/java/com/arielvieira/mc/config/SecurityConfig.java
+++ src/main/java/com/arielvieira/mc/config/SecurityConfig.java
@@ -35,7 +35,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
 
-	private static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**", "/clientes/**" };
+	private static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**" };
+	
+	private static final String[] PUBLIC_MATCHERS_POST = { "/clientes/**" };
 
 	@Override
  	protected void configure(HttpSecurity http) throws Exception {
@@ -46,6 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
  		
  		http.cors().and().csrf().disable();
  		http.authorizeRequests()
+ 			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
  			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
  			.antMatchers(PUBLIC_MATCHERS).permitAll()
  			.anyRequest().authenticated();
